8bda7087e1720f101e6d5a6fa92fa90f0335d3aa
Cristi Sandu <sandu.cristi@yahoo.com>
2023-09-10 18:50:01 +0300

    Add project files.

---
+++ lab12ex1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using lab12ex1;
+
+namespace lab12ex1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Scrieti o clasa care va implementa o structura de date generica de tipul coada. (queue)
+       Coada va functiona pe principiul FIFO (first in first out) si va avea urmatoarele metode si proprietati:
+        • Enqueue – va adauga un element la capatul cozii
+        • Dequeue – va extrage primul element din coada si il va returna ca rezultat
+        • Count – va returna numarul de elemente din coada.
+        La initializarea cozii va fi stabilita capacitatea maxima a cozii.
+        In interiorul clasei, folositi o structura de tip vector pentru a stoca datele.
+        Initializati mai multe cozi (intregi, obiecte), adaugati elemente, extrageti elemente, afisati-le.
+             */
+
+            var queue = new Queue<int>(5);
+            queue.Enqueue(10);
+            queue.Enqueue(11);
+            queue.Enqueue(12);
+            queue.Enqueue(15);
+            queue.Enqueue(1);
+
+            queue.Afisare();
+            Console.WriteLine($"Coada are {queue.Count} elemente");
+
+
+
+            var queue1 = new Queue<int>(8);
+            queue1.Enqueue(100);
+            queue1.Enqueue(111);
+            queue1.Enqueue(125);
+            queue1.Enqueue(153);
+            queue1.Enqueue(15);
+            queue1.Enqueue(36);
+            queue1.Enqueue(56);
+            queue1.Enqueue(45);
+
+
+            queue1.Afisare();
+            Console.WriteLine($"Coada are {queue1.Count} elemente");
+        }
+
+
+
+    }
+}
---
+++ lab12ex1/lab12ex1/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace lab12ex1
+{
+    class Queue<T>
+    {
+        private T[] elements;
+
+        public int Count { get; private set; } = 0;
+
+        public Queue()
+        {
+            elements = new T[4];
+        }
+
+
+        public Queue(int maxCapacity)
+        {
+
+            elements = new T[maxCapacity];
+        }
+
+        public void Enqueue(T elementToEnqueue)
+        {
+            if (Count == elements.Length)
+            {
+                /*Console.WriteLine("Nu se mai pot adauga elemente");
+                return;*/
+
+                var newVec = new T[2 * elements.Length];
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    newVec[i] = elements[i];
+                }
+
+                elements = newVec;
+
+            }
+
+            elements[Count] = elementToEnqueue;
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                return default;
+            }
+            T result = elements[0];
+
+            for (int i = 0; i < Count - 1; i++)
+            {
+                elements[i] = elements[i + 1];
+            }
+            Count--;
+
+            return result;
+        }
+
+
+        public void Afisare()
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                Console.WriteLine(elements[i]);
+            }
+        }
+    }
+}
